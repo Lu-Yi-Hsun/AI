7397888d79fa30ace841008bd4de2e247c6d15a3
lu-yi-hsun <yihsun1992@gmail.com>
2017-09-16 22:16:50 +0800

    ipynb

--- Neural Networks/2-layer Neural Network/2-layer.py
+++ Neural Networks/2-layer Neural Network/2-layer.py
@@ -24,37 +24,38 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
         outputs = activation_function(Wx_plus_b)
     return outputs,Weights,biases
 
-# Make up some real data
+def nu():
+    # define placeholder for inputs to network
+    xs = tf.placeholder(tf.float32, [None, 1])
+    ys = tf.placeholder(tf.float32, [None, 1])
+    # add hidden layer 隱藏層
+    neural_node=15
+    l1 ,Weights1,biases1= add_layer(xs, 1, neural_node, activation_function=tf.nn.relu)
+
+    l2 ,Weights2,biases2= add_layer(l1, neural_node, neural_node, activation_function=tf.nn.relu)
+    l3 ,Weights3,biases3= add_layer(l2, neural_node, neural_node, activation_function=tf.nn.relu)
+
+    # add output layer 輸出層
+    prediction ,Weights2,biases2= add_layer(l3, neural_node, 1, activation_function=None)
+
+    # the error between prediction and real data
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys-prediction), reduction_indices=[1]))
+    #loss=tf.square(ys-prediction)
+    train_step = tf.train.RMSPropOptimizer(0.01).minimize(loss)
+    # important step
+    sess = tf.Session()
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    return  sess,train_step,xs,ys,prediction
+
+
 x_data = np.linspace(-5, 10, 2000)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = 2*x_data**3*(np.sin(x_data)/2) - 0.5 + noise*300
-##plt.scatter(x_data, y_data)
-##plt.show()
-
-# define placeholder for inputs to network
-xs = tf.placeholder(tf.float32, [None, 1])
-ys = tf.placeholder(tf.float32, [None, 1])
-# add hidden layer
-neural_node=150
-l1 ,Weights1,biases1= add_layer(xs, 1, neural_node, activation_function=tf.sigmoid)
-# add output layer
-prediction ,Weights2,biases2= add_layer(l1, neural_node, 1, activation_function=None)
-
-# the error between prediction and real data
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys-prediction), reduction_indices=[1]))
-#loss=tf.square(ys-prediction)
-train_step = tf.train.RMSPropOptimizer(0.01).minimize(loss)
-# important step
-sess = tf.Session()
-# tf.initialize_all_variables() no long valid from
-# 2017-03-02 if using tensorflow >= 0.12
-if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
-    init = tf.initialize_all_variables()
-else:
-    init = tf.global_variables_initializer()
-sess.run(init)
 
 # plot the real data
+
+
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 
@@ -67,11 +68,18 @@ plt.show()
 
 
 
+
+
+
+
+# start
+sess,train_step,xs,ys,prediction=nu()
 for i in range(100000):
+
     # training
-    print(i)
+
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
-    if i % 50 == 0:
+    if i % 300 == 0:
         # to visualize the result and improvement
         try:
             ax.lines.remove(lines[0])
@@ -79,11 +87,6 @@ for i in range(100000):
             pass
         x_data2 = np.linspace(-5, 10, 5000)[:,np.newaxis]
         prediction_value = sess.run(prediction, feed_dict={xs: x_data})
-
-        #x_data2 = np.linspace(-5, 15, 5000)[:, np.newaxis]
-        #prediction_value = sess.run(prediction, feed_dict={xs: x_data2})
-
-        #print(sess.run(loss,feed_dict={xs: x_data, ys: y_data}))
 
 
         # plot the prediction
